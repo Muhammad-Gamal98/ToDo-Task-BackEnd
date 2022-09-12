22935a78b8aeacbcccf8405ae8850c0de4e8281b
Muhammad Gamal <moga1478@gmail.com>
2022-09-12 20:55:09 +0200

    listin to port and data base connect

---
+++ server.js
@@ -0,0 +1,17 @@
+require("dotenv").config();
+const mongoose = require("mongoose");
+const app = require("./app");
+
+mongoose
+  .connect(process.env.DATABASE)
+  .then(() => {
+    console.log("Data Base Connected");
+  })
+  .catch(() => {
+    process.exit(1);
+  });
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Lisiting on port ${port}`);
+});

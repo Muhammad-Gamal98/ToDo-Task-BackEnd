15e46eb1b7a7f7a56f9c0184a5f2c5ebd679fc4b
Muhammad Gamal <moga1478@gmail.com>
2022-09-12 23:17:19 +0200

    add api to app

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express");
-
+const todoRoutes = require("./Routes/todoRoutes");
 const app = express();
 
 app.use(express.json());
+
+app.use("/api/v1/todo", todoRoutes);
 
 module.exports = app;

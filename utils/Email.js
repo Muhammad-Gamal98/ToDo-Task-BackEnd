b9f93f951fbb5d0c2a66cf8903a0d691b81868de
Muhammad Gamal <moga1478@gmail.com>
2022-09-14 02:52:33 +0200

    Create Email Class

---
+++ utils/Email.js
@@ -0,0 +1,35 @@
+const nodemailer = require("nodemailer");
+class Email {
+  constructor(user) {
+    this.name = user.name;
+    this.to = user.email;
+    this.from = `Muhammad Gamal ${process.env.EMAIL_FROM}`;
+  }
+  transporter() {
+    return nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      auth: {
+        user: process.env.EMAIL_USERNAME,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+  async send(subject, text) {
+    const mailOptions = {
+      from: this.from,
+      to: this.to,
+      subject,
+      text,
+    };
+    await this.transporter().sendMail(mailOptions);
+  }
+
+  async sendVerficationEmail(text) {
+    await this.send(`Verfiy your Email`, text);
+  }
+  async sendPasswordReset(text) {
+    await this.send(`Reset your Password`, text);
+  }
+}
+module.exports = Email;

680c75e291d39587c7ae6d994c485cae2b03fb66
Muhammad Gamal <moga1478@gmail.com>
2022-09-16 16:34:42 +0200

    add handel JWT Expired error

--- Error/ErrorController.js
+++ Error/ErrorController.js
@@ -1,3 +1,5 @@
+const AppError = require("../utils/AppError");
+
 const errorDev = (err, req, res) => {
   if (req.originalUrl.startsWith("/api")) {
     return res.status(err.statusCode).json({
@@ -8,6 +10,18 @@ const errorDev = (err, req, res) => {
     });
   }
 };
+const errorProduction = (err, req, res) => {
+  if (req.originalUrl.startsWith("/api")) {
+    console.log("Error", err);
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  }
+};
+const handelJWTExpiredError = () => {
+  return new AppError("Your token has expired! Please log in again.", 401);
+};
 
 module.exports = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
@@ -16,6 +30,12 @@ module.exports = (err, req, res, next) => {
   if (process.env.NODE_ENV === "development") {
     console.log(process.env.NODE_ENV, "pp");
     errorDev(err, req, res);
+  }
+  if (process.env.NODE_ENV === "production") {
+    let error = { ...err };
+    console.log(error);
+    if (err.name === "TokenExpiredError") error = handelJWTExpiredError();
+    errorProduction(error, req, res);
   }
   next();
 };
